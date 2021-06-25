f5a8f2f2b220641430c935da2d1b7bb456a758c1
unknown <oussama.amari@sesame.com.tn>
2021-06-25 17:36:11 +0200

    add authentification

--- src/pages/AuthentifDash.js
+++ src/pages/AuthentifDash.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from 'axios';
 import  { Link } from 'react-router-dom'
 import Acceuil from './Acceuil'
+import AuthentifService from '../services/AuthentifService'
 export class AuthentifDash extends Component{
 
   constructor() {
@@ -9,38 +10,42 @@ export class AuthentifDash extends Component{
 
     this.state = {
       username: "",
-      password: ""
+      password: "", 
+      bureau :"rt"
     };
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.login = this.login.bind(this);
   }
 
+  
   handleFormSubmit = event => {
     event.preventDefault();
 
-    const endpoint = "http://localhost:8080/auth/signin";
+    const endpoint = "http://localhost:8080/login";
 
-    const enpoin1 = "localhost:8080/auth/bureau/" ;
+    const username = this.state.username;
+    const password = this.state.password;
 
     const user_object = {
-      username: this.state.username,
-      password: this.state.password
+      username: username,
+      password: password
     };
 
     axios.post(endpoint, user_object).then(res => {
-
-      
-      localStorage.setItem("authorization", res.data.accessToken); 
-      localStorage.setItem("User", res.data.username); 
-      localStorage.setItem("bureau", axios.get(enpoin1+localStorage.getItem("User")))
-      console.log("Success .....") ;
-      
-    }
-
-    
-    );
-    
+      console.log(res.data)
+      localStorage.setItem("authorization", res.data.username);
  
+    });
   };
+  componentDidMount(){
+   
+  }
+ login(){
+  console.log(this.state.bureau)
+  AuthentifService.login(this.state.username, this.state.password).then((res) =>{
+
+    localStorage.setItem("authorization" , res.data.token)
+ })
+ }
 
   onChange = (e) =>
   this.setState({ [e.target.name]: e.target.value });
@@ -57,7 +62,7 @@ export class AuthentifDash extends Component{
       
         </div>
         <div className="card-body">
-          <form onSubmit={this.handleFormSubmit} >
+      
             <div className="input-group form-group">
               <div className="input-group-prepend">
                 <span className="input-group-text"><i className="fas fa-user" /></span>
@@ -74,9 +79,9 @@ export class AuthentifDash extends Component{
               <input type="checkbox" />Remember Me
             </div>
             <div className="form-group">
-              <input type="submit" defaultValue="Login"  className="btn float-right login_btn"  />
+              <input type="submit" defaultValue="Login" onClick={this.login}  className="btn float-right login_btn"  />
             </div>
-          </form>
+      
         </div>
         <div className="card-footer">
           <div className="d-flex justify-content-center links">
--- src/services/AuthentifService.js
+++ src/services/AuthentifService.js
@@ -1,29 +1,36 @@
 
 import axios from "axios";
 
+const  API_URL = "http://localhost:8080/"
+
 class AuthentifService {
 
-    url = "http://localhost:8080/auth/"
 
-    signin = 
-    (username, password) =>{
-
-            return axios.post("http://localhost:8080/auth/signin", {username, password})
-            .then(response =>{
-                if (response.data.accessToken) {
-                    localStorage.setItem("user", JSON.stringify(response.data));
-                    
-                  }
-                  return response.data;
-
-            })
-
-            .catch(err => {
-                console.log(err);
-                throw err;
-              });
+    login(username, password){
+        return axios.post(API_URL + "login", {
+            username,
+            password
+        })
+        .then(response=>{
+            if(response.data.accessToken){
+                localStorage.setItem("user", JSON.stringify(response.data.username));
+                localStorage.setItem("token", JSON.stringify(response.data.accessToken));
+          
+            }
+          
+    
+            return response.data
+        })
+    
     }
     
+    logout(){
+        localStorage.removeItem("user")
+    }
+
+    getBureau(username){
+        return axios.get("localhost:8080/getBureau/",  + username)
+    }
     
     
      
